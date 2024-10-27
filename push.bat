5fc2b07cec74efac1b9c6d0a42950dae2933543c
mpc1991 <158753587+mpc1991@users.noreply.github.com>
2024-10-27 14:25:58 +0100

    14:25

--- push.bat
+++ push.bat
@@ -1,4 +1,15 @@
+@echo off
+setlocal
+
+rem Obtener la fecha y hora actual
+for /f "tokens=2 delims= " %%a in ('date /t') do set currentDate=%%a
+for /f "tokens=1 delims= " %%b in ('time /t') do set currentTime=%%b
+
+rem Formatear la fecha y hora
+set formattedDate=%currentDate% %currentTime%
+
+rem Comandos de git
 git add .
 git status
-git commit -m eof
+git commit -m "%formattedDate%"
 git push
