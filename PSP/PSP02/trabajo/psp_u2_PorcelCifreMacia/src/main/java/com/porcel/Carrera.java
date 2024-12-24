49098f42aae7cbb9718d842a62ff75e1faf84a05
mpc1991 <158753587+mpc1991@users.noreply.github.com>
2024-12-24 19:08:47 +0100

    update

--- PSP/PSP02/trabajo/psp_u2_PorcelCifreMacia/src/main/java/com/porcel/Carrera.java
+++ PSP/PSP02/trabajo/psp_u2_PorcelCifreMacia/src/main/java/com/porcel/Carrera.java
@@ -1,92 +1,121 @@
 package com.porcel;
 
+import java.io.IOException;
 import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.util.*;
+
 
 public class Carrera {
     private static int gonyador = 1;
     private static String missatge;
     private static int METROS_TOTALS;
-    private static String estado = "";
-    private static int cuantitatCavalls = 0;
-    private static int iteracioCavall = 0;
+    private static String estado;
+    private static int totalCavalls = 0;
+    private static List<Cavall> cavalls = new ArrayList<>();
+    private static Scanner sc = new Scanner(System.in);
 
-    public Carrera() {
-
-    }
-
-    public static void sumaGonyador(String nom) {
+    public static void sumaGonyador(Cavall cavall) throws IOException {
         switch (gonyador) {
             case 1:
                 missatge = "-----------------------------------------";
-                missatge += "\n| La primera posicio és per: " + nom + "  |";
+                missatge += "\n| La primera posicio és per: " + cavall.getNom();
                 gonyador++;
                 break;
             case 2:
-                missatge += "\n|--------------------------------------|";
-                missatge += "\n| La segona posicio és per: " + nom + "   |";
+                missatge += "\n|----------------------------------------";
+                missatge += "\n| La segona posicio és per: " + cavall.getNom();
                 gonyador++;
                 break;
             case 3:
-                missatge += "\n|--------------------------------------|";
-                missatge += "\n| La tercera posició és per: " + nom + "  |";
+                missatge += "\n|----------------------------------------";
+                missatge += "\n| La tercera posició és per: " + cavall.getNom();
                 missatge += "\n-----------------------------------------";
 
                 gonyador++;
-
                 System.out.println("\nResultat de la carrera:");
                 System.out.println(missatge);
                 System.out.println("\n");
+                String continuar= "";
 
+                while (!continuar.equalsIgnoreCase("s") && !continuar.equalsIgnoreCase("n")) {
+                    System.out.println("\nQuieres continuar la carrera (S/N)");
+                    continuar = sc.nextLine();
+                    if (!continuar.equalsIgnoreCase("s") && !continuar.equalsIgnoreCase("n")) {
+                        System.out.println("\nDebes escribir 's' o 'n'");
+                    }
+                    if (continuar.equalsIgnoreCase("N")) {
+                        Main.acabar();
+                    }
+                }
+                System.out.println("\nsaliendo del bucle while podium");
                 break;
             default:
-                System.out.println(nom + " no entra a podio");
+                System.out.println("dafault en notificarpodio" + cavall.getNom());
         }
-    }
-
-    public static void estatCarrera(String nom, int metrosFaltants, int velocitat) {
-        int totalGuiones = 100;
-        int porcentajeCompletado = 100 - ((metrosFaltants * 100) / METROS_TOTALS);
-        int guionesProgreso = porcentajeCompletado;
-
-        estado = nom + ": ";
-        if (cuantitatCavalls < 10 ) { estado += "";}
-        //if (cuantitatCavalls > 10 && cuantitatCavalls < 100) {estado += "";}
-        for (int i = 0; i < guionesProgreso; i++) {
-            if (guionesProgreso > totalGuiones) {
-                guionesProgreso = totalGuiones;
-            }
-
-            estado += "-";
-        }
-        estado += ">";
-
-        for (int i = guionesProgreso; i < totalGuiones; i++) {
-            estado += " ";
-        }
-        estado += "META! " + velocitat + "Km/h";
-        System.out.println(estado);
-        iteracioCavall++;
-        if (iteracioCavall == cuantitatCavalls) {
-            showDate();
-            iteracioCavall = 0;
-        }
-    }
-
-    public static int getGonyador() {
-        return gonyador;
     }
 
     public static void setMetrosTotals(int metrosTotals) {
         METROS_TOTALS = metrosTotals;
     }
 
-    public static void setCuantitatCavalls(int n) {
-        cuantitatCavalls = n;
+    public static void showDate() {
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+        System.out.print(simpleDateFormat.format(new Date()) + "\n");
     }
 
-    public static void showDate() {
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss" + "\n\n");
-        System.out.println(simpleDateFormat.format(new Date()));
+    public static void notificarPasada(Cavall cavall) throws IOException {
+        cavalls.add(cavall);
+        if (cavalls.size() == totalCavalls) {
+            estatCarrera2(cavalls);
+            cavalls.clear();
+        }
+    }
+
+    public static void estatCarrera2(List<Cavall> cavalls) {
+        cavalls.sort(Comparator.comparing(Thread::threadId)); //https://stackoverflow.com/questions/16252269/how-to-sort-a-list-arraylist
+        estado = "";
+
+        for (Cavall cavall : cavalls) {
+            // Calculo de guiones adaptado con ayuda de ChatGPT
+            int totalGuiones = 100;
+            int guionesProgreso = Math.min(100, Math.max(0, 100 - ((cavall.getMetrosFaltants() * 100) / METROS_TOTALS)));
+
+            estado += cavall.getNom() + ": ";
+            if (cavall.getIdCavall() < 10) {
+                estado += " ";
+            }
+
+            for (int i = 0; i < guionesProgreso; i++) {
+                if (guionesProgreso > totalGuiones) {
+                    guionesProgreso = totalGuiones;
+                }
+                estado += "-";
+            }
+            estado += ">";
+
+            for (int i = guionesProgreso; i < totalGuiones; i++) {
+                estado += " ";
+            }
+            int metrosrestantesCarrera = METROS_TOTALS - cavall.getMetrosFaltants();
+            if (metrosrestantesCarrera > METROS_TOTALS) {
+                metrosrestantesCarrera = METROS_TOTALS;
+            }
+            estado += "META! " + cavall.getVelocitat() + "Km/h - Metros recorreguts " + metrosrestantesCarrera + "/" + METROS_TOTALS;
+
+            if (cavall.getMetrosFaltants() <= 0) {
+                estado += " - FINALIZADO!";
+            }
+            estado += "\n";
+        }
+        System.out.print(estado);
+        showDate();
+    }
+
+    public static void setTotalCavalls(int n){
+        Carrera.totalCavalls = n;
+    }
+    public static void clearContador(){
+        gonyador = 1;
+
     }
 }
--- PSP/PSP02/trabajo/psp_u2_PorcelCifreMacia/src/main/java/com/porcel/Main.java
+++ PSP/PSP02/trabajo/psp_u2_PorcelCifreMacia/src/main/java/com/porcel/Main.java
@@ -1,5 +1,7 @@
 package com.porcel;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 import java.util.concurrent.Semaphore;
 
@@ -7,80 +9,95 @@ public class Main {
     private static int cuantitatCavalls;
     private static int metrosCarrera;
     private static Scanner scn = new Scanner(System.in);
-    private static Semaphore semaphore;
+    private static Semaphore semaphore = new Semaphore(1);
+    private static List<Cavall> cavalls = new ArrayList<>();
+    private static String tornarJugar;
 
     public static void main(String[] args) {
-        prepararCavalls();
+        do  {
+            tornarJugar = "";
+            calcularCuantitatCavalls();
+            calcularCuantitatMetrosCarrera();
+            prepararCavalls();
+
+            clerance();
+            System.out.println("Vols tornar a correr? (S/N)");
+            tornarJugar = scn.nextLine();
+
+        } while (tornarJugar.equalsIgnoreCase("s"));
+        System.out.println("Gracies per participar.");
     }
 
-    private static void prepararCavalls() {
+    private static void calcularCuantitatCavalls() {
         while (cuantitatCavalls < 10) {
             try {
                 System.out.println("Indica la cantidad de cavalls que participaran");
                 cuantitatCavalls = scn.nextInt();
                 scn.nextLine();
                 if (cuantitatCavalls < 10) {
-                    System.out.println("Los caballos que van a participar no pueden ser menos de 10");
+                    System.out.println("No poden correr menys de 10 cavalls");
+                } else {
+                    MonitorCavall.setTotalCavalls(cuantitatCavalls);
+                    Carrera.setTotalCavalls(cuantitatCavalls);
+                    Cavall.setTOTAL_CAVALLS(cuantitatCavalls);
                 }
             } catch (Exception e) {
-                System.out.println("La cantidad de caballos se debe indicar con un número entero.");
-                e.getMessage();
-                throw new RuntimeException(e);
+                System.out.println("La cuantitat de cavalls es té que indicar amb un nombre sencer.");
+                scn.nextLine();
             }
         }
-        semaphore = new Semaphore(1, true);
-        Carrera.setCuantitatCavalls(cuantitatCavalls);
+    }
 
+    private static void calcularCuantitatMetrosCarrera() {
         while (metrosCarrera < 100) {
             try {
-                System.out.println("Indica els KM que té la pista");
+                System.out.println("Indica els metros que té la pista");
                 metrosCarrera = scn.nextInt();
                 scn.nextLine();
                 if (metrosCarrera < 100) {
-                    System.out.println("La pista debe tener almenos 100 metros.");
+                    System.out.println("La pista té que fer al manco 100 metros.");
                 } else {
                     Carrera.setMetrosTotals(metrosCarrera);
                 }
             } catch (Exception e) {
-                System.out.println("Se debe introducir un número entero.");
-                e.getMessage();
-                throw new RuntimeException(e);
+                System.out.println("Els metros de pista es tenen que indicar am un nombre sencer.");
+                scn.nextLine();
             }
         }
+    }
 
-        while (true) {
+    private static void prepararCavalls() {
+        // Creación de hilos para cada cavallo.
+        Cavall.setCorrer(true);
             try {
                 for (int i = 1; i <= cuantitatCavalls; i++) {
-                    Cavall cavall = new Cavall("Cavall " + i, metrosCarrera, semaphore);
+                    Cavall cavall = new Cavall("Cavall " + i, metrosCarrera, semaphore, i);
+                    cavalls.add(cavall);
                     cavall.start();
-                    //cavall.wait();
                 }
-                break;
+                for (Cavall cavall : cavalls) {
+                    cavall.join();
+                }
+                System.out.println("Carrera Finalitzada!");
+
             } catch (Exception e) {
                 throw new RuntimeException(e);
             }
-        }
     }
 
-    public static boolean parar(){
-        String continuar = "";
-        boolean continuado = false;
+    public static void clerance(){
+        cavalls.clear();
+        cuantitatCavalls = 0;
+        metrosCarrera = 0;
+        Carrera.clearContador();
+    }
 
-        while (!continuar.equals("s") || !continuar.equals("n")) {
-            System.out.println("Continuar? s/n");
-            continuar = scn.nextLine();
-
-            if (continuar.equalsIgnoreCase("s")) {
-                continuado = false;
-                break;
-            } else if (continuar.equalsIgnoreCase("n")) {
-                continuado = true;
-                break;
-            } else {
-                System.out.println("Debes responder con 's' o 'n'");
+    public static void acabar(){
+        for (Cavall cavall : cavalls) {
+            while (!cavall.isInterrupted()) {
+                cavall.setCorrer(false);
+                cavall.interrupt();
             }
         }
-        return continuado;
     }
-
 }
