e73d9b45e5f526cead11675dfaf11057283ac7dc
mpc1991 <158753587+mpc1991@users.noreply.github.com>
2025-02-25 23:12:44 +0100

    25/02/2025   23:12

--- PSP/PSP04/Treball/PSP04_Porcel_Cifre_Macia/src/main/java/com/porcel/psp04_porcel_cifre_macia/HelloApplication.java
+++
@@ -1,9 +0,0 @@
-package com.porcel.psp04_porcel_cifre_macia;
-
-import jakarta.ws.rs.ApplicationPath;
-import jakarta.ws.rs.core.Application;
-
-@ApplicationPath("/api")
-public class HelloApplication extends Application {
-
-}
