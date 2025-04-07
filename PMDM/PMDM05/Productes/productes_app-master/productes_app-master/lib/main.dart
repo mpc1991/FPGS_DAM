fde60379b3048b742f7c17a7ead280cec442b5fb
mpc1991 <158753587+mpc1991@users.noreply.github.com>
2025-04-07 23:52:12 +0200

    07/04/2025   23:52

--- PMDM/PMDM05/Productes/productes_app-master/productes_app-master/lib/main.dart
+++ PMDM/PMDM05/Productes/productes_app-master/productes_app-master/lib/main.dart
@@ -1,11 +1,17 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:productes_app/firebase_options.dart';
 import 'package:productes_app/providers/login_form_provider.dart';
 import 'package:productes_app/screens/product_screen.dart';
 import 'package:productes_app/screens/screens.dart';
 import 'package:productes_app/services/services.dart';
 import 'package:provider/provider.dart';
 
-void main() => runApp(AppState());
+void main() async{
+WidgetsFlutterBinding.ensureInitialized();
+await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+runApp(AppState());
+}
 
 class AppState extends StatelessWidget {
 
@@ -30,7 +36,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Productes App',
-      initialRoute: 'home',
+      initialRoute: 'login',
       routes: {
         'login': (_) => LoginScreen(),
         'home': (_) => HomeScreen(),
--- PMDM/PMDM05/Productes/productes_app-master/productes_app-master/lib/providers/login_form_provider.dart
+++ PMDM/PMDM05/Productes/productes_app-master/productes_app-master/lib/providers/login_form_provider.dart
@@ -1,17 +1,28 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class LoginFormProvider extends ChangeNotifier {
   GlobalKey<FormState> formKey = new GlobalKey<FormState>();
-
+  //final FirebaseAuth _auth = FirebaseAuth.instance;
   String email = '';
   String password = '';
-
   bool _isLoading = false;
   bool get isLoading => _isLoading;
-  set isLoading(bool value) {
-    _isLoading = value;
-    notifyListeners();
+
+  Future<void> registerWithEmailAndPassword(String email, String password) async{
+    final user = await createUserWithEmailAndPassword(email, password);
+
   }
+
+  Future<void> createUserWithEmailAndPassword (String email, String password) async {
+    //final user = await signInWithEmailAndPassword(email, password);
+  }
+
+
+  set isLoading(bool value) {
+     _isLoading = value;
+     notifyListeners();
+   }
 
   bool isValidForm() {
     print('Valor del formulari: ${formKey.currentState?.validate()}');
